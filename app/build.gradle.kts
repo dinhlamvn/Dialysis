e85e234ccefdbb9528b96ff40efdb3d2ebe7a616
Dinh Lam <dinhlamvn353@gmail.com>
2026-07-07 19:22:54 +0700

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.dialysis.app"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.1"
     }
 
     signingConfigs {
